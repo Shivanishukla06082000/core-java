1a0307d7be0a6b7501161598ce377da9055d2c15
Shivani Shukla <138119502+Shivanishukla06082000@users.noreply.github.com>
2024-05-01 17:20:49 +0530

    Add files via upload

---
+++ AlterAlpha.java
@@ -0,0 +1,24 @@
+ class AlterAlpha
+
+ {  
+
+    public static void main(String[]args)
+
+     {
+
+       char ch ='A';
+       
+       while(ch<='Z')
+
+       {
+
+       System.out.println(" Alphabet "+" "+ch);
+       
+       ch+=2;
+
+       }
+
+     }
+ 
+
+}
